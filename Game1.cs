b1fda3a0f605a94fa4867b5b3a88bd8baad0b797
John Francis <john9francis@gmail.com>
2023-05-10 21:40:26 -0400

    got the first tile to display

--- Game1.cs
+++ Game1.cs
@@ -9,16 +9,36 @@ namespace tilemap
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D _tileTexture;
+
+        // window size
+        private int windowWidth = 1000;
+        private int windowHeight = 1000;
+
+        // objects we are going to use:
+        Tile tile1;
+
         public Game1()
         {
+            // monogame stuff
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            
+            // setting window size
+            _graphics.PreferredBackBufferWidth = windowWidth;
+            _graphics.PreferredBackBufferHeight = windowHeight;
+            _graphics.ApplyChanges();
+
+            // create tile object
+            tile1 = new Tile();
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            tile1.SetPosition(new Vector2(100,100));
+
 
             base.Initialize();
         }
@@ -28,6 +48,7 @@ namespace tilemap
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            tile1.SetTexture(Content.Load<Texture2D>("Tiles/block1"));
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +66,7 @@ namespace tilemap
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            tile1.Draw(_spriteBatch);
 
             base.Draw(gameTime);
         }
---
+++ Tile.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tilemap
+{
+    internal class Tile
+    {
+        private Texture2D _texture;
+        private Vector2 _position;
+
+        private float _hitBoxWidth;
+        private float _hitBoxHeight;
+
+        public Tile()
+        {
+ 
+        }
+
+        #region Getters and Setters
+        public void SetTexture(Texture2D texture)
+        {
+            _texture = texture;
+        }
+        public void SetPosition(Vector2 position)
+        {
+            _position = position;
+        }
+        #endregion
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(
+                _texture,
+                _position,
+                null,
+                Color.White,
+                0f,
+                new Vector2(_texture.Width / 2, _texture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f);
+
+            spriteBatch.End();
+        }
+    }
+}
